e82f77222d321fac5c0ab3c3f0573511fc60f9bc
Balaji Narayana <85923874+BALAJI141031@users.noreply.github.com>
2022-05-17 20:24:02 +0530

    feat:time type added

--- src/index.js
+++ src/index.js
@@ -7,13 +7,20 @@ const displayTime = () => {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
+  let Am_Pm;
 
-  if (hours > 12) hours -= 12;
-  if (hours === 0) hours = 1;
+  if (hours > 12) {
+    hours -= 12;
+    Am_Pm = "PM";
+  }
+  if (hours === 0) {
+    hours = 12;
+    Am_Pm = "AM";
+  }
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
-  const currentTime = `${hours} : ${minutes} : ${seconds}`;
+  const currentTime = `${hours} : ${minutes} : ${seconds} ${Am_Pm}`;
   timeEl.innerText = currentTime;
   clcokEl.appendChild(timeEl);
 };
